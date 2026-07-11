64c8bab41f4f6424eaba9377d7da314df4f5cdea
André Luiz <andreluiz@live.com>
2026-07-11 19:11:44 -0300

    criacao do mapper

---
+++ EventAPI/src/main/java/com/project/EventAPI/dto/mapper/EventMapper.java
@@ -0,0 +1,30 @@
+package com.project.EventAPI.dto.mapper;
+
+import com.project.EventAPI.dto.request.EventRequestDTO;
+import com.project.EventAPI.dto.response.EventResponseDTO;
+import com.project.EventAPI.entity.Evento;
+
+
+public class EventMapper {
+
+  public EventResponseDTO entityToDto(Evento evento) {
+    EventResponseDTO eventoresponsedto =
+        new EventResponseDTO(evento.getId(), evento.getTitulo(), evento.getDescricaoEvento(),
+            evento.getPrevisaoInicio(), evento.getPrevisaoFim(), evento.getStatus(), evento.getEndereco());
+    return eventoresponsedto;
+  }
+
+  public Evento dtoToEntity(EventRequestDTO eventrequestdto) {
+    if (eventrequestdto == null) {
+      throw new IllegalArgumentException("Event não pode ser nulo");
+    }
+    Evento evento = new Evento();
+    evento.setTitulo(eventrequestdto.titulo());
+    evento.setDescricaoEvento(eventrequestdto.descricaoEvento());
+    evento.setPrevisaoInicio(eventrequestdto.previsaoInicio());
+    evento.setPrevisaoFim(eventrequestdto.previsaoFim());
+    evento.setStatus(eventrequestdto.status());
+    evento.setEndereco(eventrequestdto.endereco());
+    return evento;
+  }
+}
--- EventAPI/src/main/java/com/project/EventAPI/dto/response/EventResponseDTO.java
+++ EventAPI/src/main/java/com/project/EventAPI/dto/response/EventResponseDTO.java
@@ -1,5 +1,11 @@
 package com.project.EventAPI.dto.response;
 
-public record EventResponseDTO(
-) {
+import java.time.LocalDateTime;
+import com.project.EventAPI.entity.Endereco;
+import com.project.EventAPI.enums.Status;
+import jakarta.validation.constraints.NotBlank;
+
+public record EventResponseDTO(Long id, @NotBlank(message = "O título não pode estar vazio") String titulo,
+    @NotBlank(message = "A descrição não pode estar vazia") String descricaoEvento, LocalDateTime previsaoInicio,
+    LocalDateTime previsaoFim, Status status, Endereco endereco) {
 }
